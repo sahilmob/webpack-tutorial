4730012c1edde8aa2b6b43ced8f4de5b62d0b93f
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-02-14 22:48:22 +0200

    add style and css loader

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 import { run } from "./app/app";
 
+import "./main.css";
+
 const alertService = new AlertService();
 const componentService = new ComponentService();
 
