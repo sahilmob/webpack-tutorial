03978741a3642c571787d57b5ae3ae06644c3c65
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-02-14 22:04:00 +0200

    use es6 imports

--- src/app/alert.service.js
+++ src/app/alert.service.js
@@ -1,4 +1,6 @@
-class AlertService {
+import { inputsAreValid } from "./utils/inputs-are-valid";
+
+export class AlertService {
   constructor() {
     this.errorBox = document.getElementById("error");
   }
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { AlertService } from "./app/alert.service";
+import { ComponentService } from "./app/component.service";
+import { run } from "./app/app";
+
+const alertService = new AlertService();
+const componentService = new ComponentService();
+
+run(alertService, componentService);
