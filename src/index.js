d9b879382dd1f044803d397a2a0ce1982e1695d1
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-02-14 22:55:00 +0200

    add sass loader and override bootstrap primary button color

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 import { run } from "./app/app";
 
-import "./main.css";
+import "./main.scss";
 
 const alertService = new AlertService();
 const componentService = new ComponentService();
