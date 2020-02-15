0afaac6c51248608f82a2066477f1b484007e87f
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-02-15 10:39:10 +0200

    extract css to .css file in production

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,5 +8,13 @@ module.exports = merge(common, {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
   }
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const common = require("./webpack.common");
 
@@ -10,5 +11,16 @@ module.exports = merge(common, {
     filename: "[name].[contentHash].bundle.js",
     path: path.resolve(__dirname, "dist")
   },
-  plugins: [new CleanWebpackPlugin()]
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }
+    ]
+  }
 });
